a5ff039710bbd2c66f0ed1940eae7e5467a7c69c
root <root@borjarafols.vservers.es>
2016-08-29 14:28:38 +0200

    First commit

---
+++ src/components/products/product.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      	<h2>Product tittttttle</h2>
+      	
+      </div>
+    );
+  }
+}
+
+export default Product;
---
+++ test/product_spec.js
@@ -0,0 +1,10 @@
+import {expect} from 'chai';
+import {List} from 'immutable';
+
+describe('Component: ProductList', () => {
+	
+	it('should render without exploding', () => {
+		expect(1).to.equal(1)
+	})
+	
+});
